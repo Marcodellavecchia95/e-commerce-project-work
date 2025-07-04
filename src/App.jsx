df855a4482de239448dd8fe876f5b99a30cf591f
arielmusa <arielmusainfo@gmail.com>
2025-07-04 14:53:58 +0200

    feat: add stripe checkout form and return page components

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,18 @@
-export default function App() {
-  return <></>;
-}
+import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router";
+import CheckoutForm from "./assets/components/checkout/CheckoutForm";
+import ReturnPage from "./assets/components/checkout/ReturnPage";
+
+const App = () => {
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route path="/checkout" element={<CheckoutForm />} />
+          <Route path="/return" element={<ReturnPage />} />
+        </Routes>
+      </Router>
+    </div>
+  );
+};
+
+export default App;
